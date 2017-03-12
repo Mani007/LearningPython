f8bc55fbc8a5a39c0ce493327d558e3766ec92fb
Mohnish <mohnish.a007@gmail.com>
2017-03-12 17:34:54 +0530

    Commented microsoft visual studio server setting

--- flaskpractice/flaskpractice/app.py
+++ flaskpractice/flaskpractice/app.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
-wsgi_app = app.wsgi_app
+#wsgi_app = app.wsgi_app
 
 
 @app.route('/')
@@ -16,10 +16,10 @@ def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
+ #   import os
+ #  HOST = os.environ.get('SERVER_HOST', 'localhost')
+ #  try:
+ #      PORT = int(os.environ.get('SERVER_PORT', '5555'))
+ #  except ValueError:
+ #      PORT = 5555
     app.run(HOST, PORT)
