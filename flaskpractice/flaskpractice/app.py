7279525a68f6d354491e29d087476982d0409236
Mohnish <mohnish.a007@gmail.com>
2017-03-12 23:01:27 +0530

    Render Bootstrap template successfully. Make sure you have started/activeted virtual environment in command prompt and then run "python app.py"

--- flaskpractice/flaskpractice/app.py
+++ flaskpractice/flaskpractice/app.py
@@ -3,17 +3,18 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 #wsgi_app = app.wsgi_app
-
+app.config['DEBUG'] = True
 
 @app.route('/')
 def hello():
     """Renders a sample page."""
-    return "Hello World!"
+    #return "Hello World!"
+    return render_template("index.html")
 
 if __name__ == '__main__':
  #  import os
@@ -22,4 +23,4 @@ if __name__ == '__main__':
  #      PORT = int(os.environ.get('SERVER_PORT', '5555'))
  #  except ValueError:
  #      PORT = 5555
-    app.run(HOST, PORT)
+    app.run()
